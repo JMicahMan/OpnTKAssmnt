935456f85e1fe61cc1ab361791bcd622fdb09a14
JMicahMan <j.micah.manzanares@gmail.com>
2019-10-30 17:54:37 -0500

    asdgr

--- OpnTKAssmnt/Window.cs
+++ OpnTKAssmnt/Window.cs
@@ -3,6 +3,7 @@ using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL4;
 using OpenTK.Input;
+//using OpnTKAssmnt.OpnTKAssmnt;
 
 namespace OpnTKAssmnt
 {
@@ -68,9 +69,39 @@ namespace OpnTKAssmnt
         private int VaoModel;
         private int VaoLamp;
 
-        //private Shader LampShader;
-        public Window(int W, int H, string Title)
+        private Shader LampShader;
+        private Shader LightingShader;
+
+        private Camera TheCamera;
+        private bool FirstMove = true;
+        private Vector2 LastPos;
+
+        public Window(int W, int H, string Title) : base(W, H, GraphicsMode.Default, Title)
         {
+
         }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+            GL.Enable(EnableCap.DepthTest);
+
+            VertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, Vertices.Length * sizeof(float), Vertices, BufferUsageHint.StaticDraw);
+
+            LightingShader = new Shader("Shades/shade.vert", "Shade/lights.frag");
+            LampShader = new Shader("Shades/shade.vert", "Shades/shade.frag");
+
+            VaoModel = GL.GenVertexArray();
+            GL.BindVertexArray(VaoModel);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+
+            //Stoped on line 93 Continue on line 95
+            base.OnLoad(e);
+        }
+
     }
+    
 }
